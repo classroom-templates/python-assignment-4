3bd857184ecf5be1b0e6a17c5cbef414097ff0c5
Alexander Katrompas <alexander@aiintegratedclassroom.com>
2026-09-04 19:11:03 +0300

    Initialize main.py with basic structure

---
+++ main.py
@@ -0,0 +1,12 @@
+# don't forget your comment header here
+
+
+def main():
+    # don't forget your function header here
+
+    # Erase the pass and write your program here
+    pass
+
+
+if __name__ == "__main__":
+    main()
